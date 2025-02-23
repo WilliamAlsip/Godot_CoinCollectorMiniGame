6a0711b0879e1cd59e7772e7a9e2657cea76e820
William Alsip <williamalsip.contact@gmail.com>
2025-02-23 03:19:42 -0600

    Commit #003 - Coins & Movement
    Created coins scene and associated script.
    
    Amended player move speed.
    
    Changed controls to WASD

--- player.gd
+++ player.gd
@@ -1,24 +1,26 @@
 extends CharacterBody2D
 
+var move_speed : float = 200
+
 func _physics_process(delta: float) -> void:
 	
 	velocity.x = 0
 	velocity.y = 0
 	
 	# VELOCITY BASED ON KEY INPUTS, LEFT RIGHT UP DOWN///CHANGE THIS LATER FOR WASD
-	if Input.is_key_pressed(KEY_LEFT):
+	if Input.is_key_pressed(KEY_A):
 		velocity.x -= 1
 		
-	if Input.is_key_pressed(KEY_RIGHT):
+	if Input.is_key_pressed(KEY_D):
 		velocity.x += 1
 		
-	if Input.is_key_pressed(KEY_UP):
+	if Input.is_key_pressed(KEY_W):
 		velocity.y -= 1
 		
-	if Input.is_key_pressed(KEY_DOWN):
+	if Input.is_key_pressed(KEY_S):
 		velocity.y += 1
 		
 		
-	velocity *= 50
+	velocity *= move_speed
 	
 	move_and_slide()
