df4b3be80dbd51c4d3c1861c341ff431cda3e1e8
William Alsip <williamalsip.contact@gmail.com>
2025-02-23 02:57:35 -0600

    Commit #001
    Player movement and speed multiplier

--- player.gd
+++ player.gd
@@ -5,3 +5,20 @@ func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	velocity.y = 0
 	
+	# VELOCITY BASED ON KEY INPUTS, LEFT RIGHT UP DOWN///CHANGE THIS LATER FOR WASD
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= 1
+		
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += 1
+		
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 1
+		
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 1
+		
+		
+	velocity *= 50
+	
+	move_and_slide()
